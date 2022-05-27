70a480cf409fc539858aa89f33ceb9205420630d
kennybrodriguez <39038098+kennybrodriguez@users.noreply.github.com>
2022-05-27 15:02:40 -0500

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -55,7 +55,6 @@ function updateFilters() {
   // 7. Use this function to filter the table when data is entered.
   function filterTable() {
 
-    let date = d3.select("#datetime").property("value");
   
     // 8. Set the filtered data to the tableData.
     let filteredData = tableData;
